8436afa4fa6a5f0d98d412d5d32c0bb082d3e930
Timothy Hawea Anthony Whatley <90054174@learn.cpp.ac.nz>
2020-06-24 14:51:53 +1200

    Editing URLS for home page

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.urls import include # Use include() to add URLS from the catalog application and authentication system
 
 
@@ -12,4 +15,9 @@ urlpatterns = [
 # Have added url path to include petregistry urls - Tim
 urlpatterns += [
     path('PetRegistry/', include('PetRegistry.urls')),
+    #Added URL maps to redirect the base URL to our application - Tim
+    path('', RedirectView.as_view(url='PetRegistry/', permanent=True)),
 ]
+
+# Use static() to add url mapping to serve static files during development (only) - Tim
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
