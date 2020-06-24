352bfee9eebcb63e00300542bb36254875ad1590
Timothy Hawea Anthony Whatley <90054174@learn.cpp.ac.nz>
2020-06-24 15:09:12 +1200

    Merge branch 'master' of https://github.com/LucianEarl/TOKB-Pet-registry

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-# from . import views
+from . import views
 # from django.urls import include # Use include() to add URLS from the catalog application and authentication system
 
 
@@ -15,12 +15,12 @@ urlpatterns = [
     # path('home/', home.site.urls),
 ]
 
-# Have added url path to include petregistry urls - Tim
-urlpatterns += [
-    path('PetRegistry/', include('PetRegistry.urls')),
-    #Added URL maps to redirect the base URL to our application - Tim
-    path('', RedirectView.as_view(url='PetRegistry/', permanent=True)),
-]
-
-# Use static() to add url mapping to serve static files during development (only) - Tim
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# # Have added url path to include petregistry urls - Tim
+# urlpatterns += [
+#     path('PetRegistry/', include('PetRegistry.urls')),
+#     #Added URL maps to redirect the base URL to our application - Tim
+#     path('', RedirectView.as_view(url='PetRegistry/', permanent=True)),
+# ]
+#
+# # Use static() to add url mapping to serve static files during development (only) - Tim
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
