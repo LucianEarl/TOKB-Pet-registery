ffc70ad9d2483753bea66bf2e97e0950310a2611
Timothy Hawea Anthony Whatley <90054174@learn.cpp.ac.nz>
2020-06-24 14:46:41 +1200

    added in URl pattern

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -1,18 +1,4 @@
-"""PetLivesMatter URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 # from django.urls import include # Use include() to add URLS from the catalog application and authentication system
@@ -24,6 +10,6 @@ urlpatterns = [
 ]
 
 # Have added url path to include petregistry urls - Tim
-# urlpatterns += [
-#     path('PetRegistry/', include('PetRegistry.urls')),
-# ]
+urlpatterns += [
+    path('PetRegistry/', include('PetRegistry.urls')),
+]
