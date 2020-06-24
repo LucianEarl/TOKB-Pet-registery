68b2931ad047d471e608dcb9d1598b196857e73e
William Lucian Earl <92060031@learn.cpp.ac.nz>
2020-06-24 15:09:17 +1200

    saved

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -4,12 +4,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-<<<<<<< HEAD
 from PetRegistry import views
-=======
-from django.urls import path
-# from . import views
->>>>>>> 53f8655ca7b082a88d53fd4ac7ee7e59bde93a6c
 # from django.urls import include # Use include() to add URLS from the catalog application and authentication system
 
 
