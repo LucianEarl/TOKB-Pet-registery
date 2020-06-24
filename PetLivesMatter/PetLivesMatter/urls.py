a774bcebe06b1f385758af7928f5c53cb82ff4a6
Timothy Hawea Anthony Whatley <90054174@learn.cpp.ac.nz>
2020-06-24 14:35:58 +1200

    Changed home URl and view, is incorrect, do not use

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from django.urls import include # Use include() to add URLS from the catalog application and authentication system
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('home/', home.site.urls),
 ]
+
+# Have added url path to include petregistry urls - Tim
+# urlpatterns += [
+#     path('PetRegistry/', include('PetRegistry.urls')),
+# ]
