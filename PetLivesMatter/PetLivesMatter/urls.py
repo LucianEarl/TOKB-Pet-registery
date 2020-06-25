201e052977d6092372a5a5ce48f91b273fbc40b0
William Lucian Earl <92060031@learn.cpp.ac.nz>
2020-06-25 12:26:24 +1200

    fix about

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -11,7 +11,7 @@ from PetRegistry import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('about/', views.home, name='about'),
+    path('about/', views.about, name='about'),
     # path('home/', home.site.urls),
 ]
 
