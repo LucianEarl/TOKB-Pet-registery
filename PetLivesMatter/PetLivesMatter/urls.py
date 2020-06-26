4891221448275bbc7c352fd5759e3d0c0855ba13
Timothy Hawea Anthony Whatley <90054174@learn.cpp.ac.nz>
2020-06-26 13:37:49 +1200

    Final Copy yo! Shit is working, break me

--- PetLivesMatter/PetLivesMatter/urls.py
+++ PetLivesMatter/PetLivesMatter/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import url
 from django.urls import include
 from PetRegistry import views
 from django.views.generic import RedirectView
-from register import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,18 +11,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('donate/', views.donate, name='donate'),
-    path("register/", views.register, name="register"),  # <-- added
-]
-
-urlpatterns += [
-    path('', RedirectView.as_view(url='/home/')),
-    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-#Add Django site authentication urls (for login, logout, password management)
+# Add Django site authentication urls (for login, logout, password management)
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
