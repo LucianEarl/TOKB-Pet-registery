f6f5b69e840bc45d88080e21c0a4a2c74d3b1a40
Troy Brown <90054008@learn.cpp.ac.nz>
2020-07-01 14:16:16 +1200

    Commit

--- PetLivesMatter/Tests/test_urls.py
+++ PetLivesMatter/Tests/test_urls.py
@@ -1,3 +1,4 @@
+"""
 from django.urls import reverse
 from budget.models import project, Category, Expense
 import json
@@ -18,3 +19,18 @@ class TestViews(TestCase):
 
     def test_project_list
         test_project_list_GET
+"""
+from django.test import TestCase, client
+from django.urls import reverse
+from budget.models import Project,
+
+
+class TestViews(TestCase):
+
+    def test_project_list_GET(self):
+        response = Client()
+
+        response = client.get(reverse('list'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 900)
